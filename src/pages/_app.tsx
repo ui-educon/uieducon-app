83b912c1f5a6422ae2a7ace5af326eae008103b0
vmbansal2001 <vmbansal2001@gmail.com>
2023-11-01 00:00:45 +0530

    Forgot password implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import Layout from "@/components/Layout/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "@/css/globalStyles.css";
+import { Toaster } from 'react-hot-toast'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Toaster containerClassName="mt-20" />
     </Layout>
   );
 }
